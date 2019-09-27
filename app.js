099291718a3cf447caed9445cb0aed3b25177168
Adeeb Ahmad <adeebahmad0010@gmail.com>
2019-09-27 21:52:35 +0500

    Todo List V1 Completed

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + '/date.js')
 const app = express();
 app.use(express.static("public"));
-let items=['Buy Food'];
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }));
+const items=['Buy Food', 'Cook Food' , 'Eat Food'];
+const listItems=[];
 
 app.get("/", function(req, res){
-  
-  let today = new Date();
-  let options = {
-    weekday: 'long', day: '2-digit' , month: 'long' , year:'numeric'
-  }
-  let day =today.toLocaleDateString('en-US', options)
-
+  const day = date.getDate();
   res.render('index' , {dayOfWeek : day,newListItem: items})
 });
 
 app.post('/', function (req,res) {  
   input = req.body.input;
-  items.push(input);
-  res.redirect('/')
+  
+  if(req.body.list === 'Work List'){
+    listItems.push(input)
+    res.redirect('/work')
+  }
+  else{
+    items.push(input);
+    res.redirect('/')
+  }
 })
+
+app.get('/work', function (req,res) {  
+  res.render('index',{dayOfWeek: 'Work List', newListItem: listItems})
+})
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
-});
+});  
